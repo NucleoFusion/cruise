5dbbbd3393c57f36b07b5f92c6505d2516a42888
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-01-07 21:11:42 +0530

    feat: Adding Container specifics for docker runtime
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- pkg/runtimes/docker/docker.go
+++ pkg/runtimes/docker/docker.go
@@ -1,11 +1,10 @@
-package runtimes
+package dockerruntime
 
 import (
 	"context"
 	"log"
 
 	"github.com/NucleoFusion/cruise/pkg/types"
-	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/volume"
@@ -31,51 +30,7 @@ func NewDockerClient() *DockerRuntime {
 
 func (s *DockerRuntime) Name() string { return "docker" }
 
-func (s *DockerRuntime) Containers(ctx context.Context) (*[]types.Container, error) {
-	dockerCtr, err := s.Client.ContainerList(context.Background(), container.ListOptions{All: true})
-	if err != nil {
-		return nil, err
-	}
-
-	// Type Assert
-	cnt := make([]types.Container, 0, len(dockerCtr))
-	for _, v := range dockerCtr {
-		state := types.ContainerState(v.State)
-
-		// asserting ports
-		ports := make([]types.ContainerPort, 0, len(v.Ports))
-		for _, p := range v.Ports {
-			ports = append(ports, types.ContainerPort{
-				ContainerPort: p.PrivatePort,
-				HostPort:      p.PublicPort,
-				Protocol:      p.Type,
-			})
-		}
-
-		// asserting mounts
-		mounts := make([]types.ContainerMount, 0, len(v.Ports))
-		for _, m := range v.Mounts {
-			mounts = append(mounts, types.ContainerMount{
-				Source:      m.Source,
-				Destination: m.Destination,
-				ReadOnly:    !m.RW,
-			})
-		}
-
-		cnt = append(cnt, types.Container{
-			Name:    v.Names[0],
-			ID:      v.ID,
-			Image:   v.Image,
-			Created: v.Created,
-			Ports:   ports,
-			State:   state,
-			Mounts:  mounts,
-			Labels:  v.Labels,
-		})
-	}
-
-	return &cnt, nil
-}
+// Container Specifics
 
 func (s *DockerRuntime) Images(ctx context.Context) (*[]types.Image, error) {
 	dockerImg, err := s.Client.ImageList(context.Background(), image.ListOptions{All: true})
