46ba18d873ad9d5bcc85e1d33fa6b3f199511409
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-01-20 18:36:02 +0530

    feat: adding statcard for container details and images
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- pkg/runtimes/docker/docker.go
+++ pkg/runtimes/docker/docker.go
@@ -5,7 +5,6 @@ import (
 	"log"
 
 	"github.com/NucleoFusion/cruise/pkg/types"
-	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/volume"
 	"github.com/docker/docker/client"
@@ -29,29 +28,6 @@ func NewDockerClient() *DockerRuntime {
 }
 
 func (s *DockerRuntime) Name() string { return "docker" }
-
-// Container Specifics
-
-func (s *DockerRuntime) Images(ctx context.Context) (*[]types.Image, error) {
-	dockerImg, err := s.Client.ImageList(context.Background(), image.ListOptions{All: true})
-	if err != nil {
-		return nil, err
-	}
-
-	// Type Assert
-	img := make([]types.Image, 0, len(dockerImg))
-	for _, v := range dockerImg {
-		img = append(img, types.Image{
-			ID:            v.ID,
-			Tags:          v.RepoTags,
-			Size:          v.Size,
-			CreatedAt:     v.Created,
-			NumContainers: v.Containers,
-		})
-	}
-
-	return &img, nil
-}
 
 func (s *DockerRuntime) Network(ctx context.Context) (*[]types.Network, error) {
 	dockerNet, err := s.Client.NetworkList(context.Background(), network.ListOptions{})
