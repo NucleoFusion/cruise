117a4f803e05bf9c89fa350b4efd5c2b1179faed
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-01-23 11:51:39 +0530

    feat: Adding Metadat for statCards
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- pkg/types/details.go
+++ pkg/types/details.go
@@ -10,3 +10,13 @@ type StatCard interface {
 	Title() string
 	Stats(ctx context.Context, cli *client.Client) (*map[string]string, error)
 }
+
+type StatMeta struct {
+	TotalRows    int
+	TotalColumns int
+	SpanMap      *map[string]struct {
+		Rows    int
+		Columns int
+		Index   int
+	}
+}
