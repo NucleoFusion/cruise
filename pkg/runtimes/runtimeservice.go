43be0753ec9d878bea76060b7ba5ecb4870b0330
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-01-28 19:18:44 +0530

    feat: Adding Get fn for Runtime Service
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- pkg/runtimes/runtimeservice.go
+++ pkg/runtimes/runtimeservice.go
@@ -1,9 +1,14 @@
 package runtimes
 
 import (
+	"context"
+	"errors"
+
 	"github.com/cruise-org/cruise/pkg/config"
+	"github.com/cruise-org/cruise/pkg/types"
 )
 
+// RuntimeService is an aggregating service for multiple runtimes
 type RuntimeService struct {
 	Runtimes map[string]Runtime
 }
@@ -15,4 +20,72 @@ func NewRuntimeService() {
 	for _, v := range runtimeNames {
 		runtimes[v] = runtimeMap[v]()
 	}
+}
+
+func (s *RuntimeService) Containers(ctx context.Context) (*[]types.Container, error) {
+	cnts := make([]types.Container, 0)
+	errs := make([]error, 0)
+
+	for _, v := range s.Runtimes {
+		res, err := v.Containers(ctx)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		cnts = append(cnts, *res...)
+	}
+
+	return &cnts, errors.Join(errs...)
+}
+
+func (s *RuntimeService) Volumes(ctx context.Context) (*[]types.Volume, error) {
+	cnts := make([]types.Volume, 0)
+	errs := make([]error, 0)
+
+	for _, v := range s.Runtimes {
+		res, err := v.Volumes(ctx)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		cnts = append(cnts, *res...)
+	}
+
+	return &cnts, errors.Join(errs...)
+}
+
+func (s *RuntimeService) Networks(ctx context.Context) (*[]types.Network, error) {
+	cnts := make([]types.Network, 0)
+	errs := make([]error, 0)
+
+	for _, v := range s.Runtimes {
+		res, err := v.Networks(ctx)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		cnts = append(cnts, *res...)
+	}
+
+	return &cnts, errors.Join(errs...)
+}
+
+func (s *RuntimeService) Images(ctx context.Context) (*[]types.Image, error) {
+	cnts := make([]types.Image, 0)
+	errs := make([]error, 0)
+
+	for _, v := range s.Runtimes {
+		res, err := v.Images(ctx)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		cnts = append(cnts, *res...)
+	}
+
+	return &cnts, errors.Join(errs...)
 }
