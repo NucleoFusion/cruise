c13da9ac6609e4235f5371363b39b1b15f3e42cc
NucleoFusion <lakshit.singh.mail@gmail.com>
2026-01-30 20:00:00 +0530

    feat: Adding implementation of Runtime Service for Images and Containers
    Signed-off-by: NucleoFusion <lakshit.singh.mail@gmail.com>

--- pkg/runtimes/runtimeservice.go
+++ pkg/runtimes/runtimeservice.go
@@ -3,6 +3,7 @@ package runtimes
 import (
 	"context"
 	"errors"
+	"os/exec"
 
 	"github.com/cruise-org/cruise/pkg/config"
 	"github.com/cruise-org/cruise/pkg/types"
@@ -88,4 +89,68 @@ func (s *RuntimeService) Images(ctx context.Context) (*[]types.Image, error) {
 	}
 
 	return &cnts, errors.Join(errs...)
+}
+
+func (s *RuntimeService) StartContainer(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].StartContainer(ctx, id)
+}
+
+func (s *RuntimeService) StopContainer(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].StopContainer(ctx, id)
+}
+
+func (s *RuntimeService) PauseContainer(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].PauseContainer(ctx, id)
+}
+
+func (s *RuntimeService) UnpauseContainer(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].UnpauseContainer(ctx, id)
+}
+
+func (s *RuntimeService) RestartContainer(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].RestartContainer(ctx, id)
+}
+
+func (s *RuntimeService) RemoveContainer(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].RemoveContainer(ctx, id)
+}
+
+func (s *RuntimeService) ExecContainer(ctx context.Context, runtime string, id string) *exec.Cmd {
+	return s.Runtimes[runtime].ExecContainer(ctx, id)
+}
+
+func (s *RuntimeService) PortsMap(ctx context.Context, runtime string, id string) (map[string][]string, error) {
+	return s.Runtimes[runtime].PortsMap(ctx, id)
+}
+
+func (s *RuntimeService) ContainerDetails(ctx context.Context, runtime string, id string) ([]types.StatCard, *types.StatMeta) {
+	return s.Runtimes[runtime].ContainerDetails(ctx, id)
+}
+
+func (s *RuntimeService) PruneImages(ctx context.Context, runtime string) error {
+	var errs []error
+	for _, v := range s.Runtimes {
+		err := v.PruneImages(ctx)
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+func (s *RuntimeService) RemoveImage(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].RemoveImage(ctx, id)
+}
+
+func (s *RuntimeService) PullImage(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].PullImage(ctx, id)
+}
+
+func (s *RuntimeService) ImageLayers(ctx context.Context, runtime string, id string) (string, error) {
+	return s.Runtimes[runtime].ImageLayers(ctx, id)
+}
+
+func (s *RuntimeService) PushImage(ctx context.Context, runtime string, id string) error {
+	return s.Runtimes[runtime].PushImage(ctx, id)
 }
